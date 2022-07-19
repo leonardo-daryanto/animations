b0b226e71c7e06788aca4ad8f91c8379882c2210
leonardo-daryanto <103314397+leonardo-daryanto@users.noreply.github.com>
2022-07-19 12:46:42 +0700

    Update pxt.json, main.blocks, main.ts, custom.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,37 @@
-basic.forever(function () {
-	
-})
+enum switchEnum {
+    //% block="yes"
+    Yes,
+    //% block="no"
+    No,
+}
+
+namespace images {
+    /**
+     *  make a animation
+     */
+    //% block
+    //% group="animation"
+    export function createAnimation(makeAnimation: TemplateStringsArray): void {
+
+        let animation = [makeAnimation]
+    }
+    /**
+     *  makes a animation
+     */
+    //% block
+    //% group="getAnimation"
+    export function openAnimation(doAnimation: switchEnum): void {
+        
+        
+        if (doAnimation == 0) {
+            
+            images.createAnimation
+        } else if (doAnimation == 1) {
+            
+            basic.clearScreen()
+        }
+
+        
+
+    }
+}
